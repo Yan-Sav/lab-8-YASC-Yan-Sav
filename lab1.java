9f6ea279349869b526cbd6afca0858dcc36a3d6d
Yan-Sav <124152793+Yan-Sav@users.noreply.github.com>
2024-03-13 12:36:35 +0000

    Add files via upload
    Adding files from the main OOP repo to have a base to start with (more files to be added).

---
+++ lab1.java
@@ -0,0 +1,8 @@
+public class lab1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Woof. I am Misty");
+        System.out.println("Woof. I am Lucy");
+        System.out.println("cats are from space");
+    }
+}
