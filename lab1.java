09f46eae27e9a3d6e7953e4df8700e28a6500afd
Yan-Sav <124152793+Yan-Sav@users.noreply.github.com>
2024-03-14 11:43:15 +0000

    Delete lab1.java
    Deleting wrong files for the lab.

--- lab1.java
+++
@@ -1,8 +0,0 @@
-public class lab1 {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Woof. I am Misty");
-        System.out.println("Woof. I am Lucy");
-        System.out.println("cats are from space");
-    }
-}
